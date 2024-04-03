a02305b83c71142aaed28970364c881dc5870dbb
Srivalli-2002 <98386777+Srivalli-2002@users.noreply.github.com>
2024-04-03 21:18:18 +0530

    Update S.java

--- April3_Collections and Generic/S.java
+++ April3_Collections and Generic/S.java
@@ -1,25 +1,27 @@
 package collectionsandgeneric;
 
-public class S {
-	public String s;
+public class S<N> implements Comparable {
+	public N s;
 
 	public S() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
 
-	public S(String s) {
+	public S(N s) {
 		super();
 		this.s = s;
 	}
 
-	public String getS() {
+	public N getS() {
 		return s;
 	}
 
-	public void setS(String s) {
+	public void setS(N s) {
 		this.s = s;
 	}
+
+	@Override
 	public int compareTo(Object o)
 	{
 		if(this.s.equals(o))
