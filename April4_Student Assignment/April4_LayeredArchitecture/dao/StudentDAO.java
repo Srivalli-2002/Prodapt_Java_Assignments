1d4529d3064751f0297ba5d69edbcb35c1045ffb
Srivalli-2002 <98386777+Srivalli-2002@users.noreply.github.com>
2024-04-04 17:24:14 +0530

    Add files via upload

---
+++ April4_Student Assignment/April4_LayeredArchitecture/dao/StudentDAO.java
@@ -0,0 +1,19 @@
+package studentassignment.dao;
+
+import java.util.List;
+
+import studentassignment.exceptions.DataNotPresentException;
+import studentassignment.exceptions.StudentNotFoundException;
+import studentassignment.model.Student;
+
+public interface StudentDAO {
+
+	//Add student
+	//Access Modifier Return Type Name of the method
+	//(Method accepts Student type argument)
+	public String addStudent(Student student) throws DataNotPresentException;
+	public List<Student> displayStudent();
+	public List<Student> displayStudentByCity(String city) throws StudentNotFoundException;
+	public List<Student> displayStudentById(Integer id) throws StudentNotFoundException;
+	
+}
